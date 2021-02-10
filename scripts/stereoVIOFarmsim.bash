4372d51416815a3cab134944e88a502bdbf407f2
Milo Knowles <mknowles@mit.edu>
2021-02-10 09:56:15 -0500

    Update params/flags for FarmSim

--- scripts/stereoVIOFarmsim.bash
+++ scripts/stereoVIOFarmsim.bash
@@ -4,7 +4,8 @@
 
 # Specify path of the EuRoC dataset.
 # The path can be absolute, or relative to this file location.
-DATASET_PATH="/home/milo/datasets/Unity3D/farmsim/euroc_01_seafloor/"
+# NOTE(milo): No trailing slash!
+DATASET_PATH="/home/milo/datasets/Unity3D/farmsim/euroc_test1"
 
 # Specify: 0 to run on EuRoC data, 1 to run on Kitti (not supported)
 DATASET_TYPE=0
